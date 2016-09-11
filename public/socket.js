5d8aa5696a19561fbe28f58b8a73a623eed8c26c
damianwajser <wajser@gmail.com>
2016-09-11 19:00:20 -0300

    lala

--- public/socket.js
+++ public/socket.js
@@ -4,13 +4,16 @@ function Socket(movimiento, gano, reinicie){
   var socket = io();
 
   self.play = function(position){
+    //emite hacia el server con la key movimiento
     socket.emit("movimiento", {position:position});
     movimiento(self.figura(), position);
   }
   self.figura = function(){
     return self.juego ? "X":"O";
   }
+
   socket.on("connect", function(){
+    //escucha al server con la key init
     socket.on("init", function(data){
       self.juego = data.figure;
     })
