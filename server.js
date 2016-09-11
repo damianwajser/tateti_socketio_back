497159e7918039721d12bedfcc4a7dda0389a84b
damianwajser <wajser@gmail.com>
2016-09-11 16:21:59 -0300

    boostrap

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var http = require ("http");
-
-
 var app = express();
 
-var server = app.createServer(app);
+var server = http.createServer(app);
+
+app.get("/", function (req, res){
+  res.sendFile("./index.html", {"root":__dirname});
+});
 
 
+server.listen(2000);
