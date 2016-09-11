e97372b26879b117a0079ccd0925695b45be9f9b
ghi <gustavoghioldi@gmail.com>
2016-09-11 17:02:29 -0300

    grid listo

--- public/main.js
+++ public/main.js
@@ -10,7 +10,7 @@ function build_cat(){
 }
 
 function build_item(i){
-    return "<div class='cat-element col-xs-4'></div>";
+    return "<div class='cat-element col-md-4' id='elemento-'"+i+"></div>";
 }
 
 build_cat();
