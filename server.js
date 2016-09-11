10b584ce418ca9f56146017c0bba2a6d753f71a6
damianwajser <wajser@gmail.com>
2016-09-11 16:35:46 -0300

    server cors and morgan

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require("express");
+var cors = require('cors');
 var http = require ("http");
-var app = express();
+var morgan = require('morgan');
 
+var app = express();
 var server = http.createServer(app);
+
+//seteo para loguear todos los reqest  responses
+app.use(morgan('dev'));
+
+//creo la carpeta para contenido estatico
+app.use("static", express.static("public"));
+
+//seteo los cors para todos los servicios
+app.all("*", cors());
 
 app.get("/", function (req, res){
   res.sendFile("./index.html", {"root":__dirname});
 });
-
 
 server.listen(2000);
